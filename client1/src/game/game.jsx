0f119d93ab71ba31095e2d02b73e55d9049af8a1
karamnaser <karam_m_n@hotmail.com>
2020-01-25 18:28:05 +0200

    master

--- client1/src/game/game.jsx
+++ client1/src/game/game.jsx
@@ -44,7 +44,7 @@ class Game extends  React.Component{
     }
     sendcurrentgamedetails(){
             sendgamenumbers({gameid:this.state.gameid,number:this.state.current_number})
-             sendgameDetails({username:this.state.userid,
+             sendgameDetails({userid:this.state.user[0]["id"],
                 minnumber:this.state.min_number,
                  maxnumber:this.state.max_number,
             choosennumber:this.state.choosen_number})
